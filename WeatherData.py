becf150998745be1153a21fa8a30ad2ea2e3adf3
xenophorus <xenophorus@gmail.com>
2024-09-11 18:58:24 +1000

    1

---
+++ WeatherData.py
@@ -0,0 +1,41 @@
+import requests
+import csv
+import asyncio
+
+from bs4 import BeautifulSoup
+
+
+class WeatherData:
+
+    def __init__(self, days):
+        self.days = days
+
+    def run(self):
+        asyncio.run(self.get_weather_data())
+
+    def get_url(self, data_file):
+        with open(data_file, 'r', encoding='utf-8') as f:
+            for line in f:
+                yield line
+
+    async def get_data(self, url):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0"
+        }
+        data = requests.get(url=url, headers=headers).text
+        return data
+
+    def get_info(self, html):
+        soup = BeautifulSoup(html, "lxml")
+        forecast = soup.findAll('div', attrs={'class': 'forecast'})
+        forecast[0].findAll('h3')
+        print(1)
+
+    async def get_weather_data(self):
+        csv_data = "./towns.csv"
+        with open(csv_data, 'r', encoding='utf-8') as csv_file:
+            lines = csv.reader(csv_file)
+            next(lines)
+            for line in lines:
+                html = await self.get_data(line[2])
+                self.get_info(html)
--- main.py
+++ main.py
@@ -1,45 +1,30 @@
-import requests
-import csv
-import asyncio
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtCore import QSize, Qt
 
-from bs4 import BeautifulSoup
+'''
+TODO
+Weather class gets only one forecast at a time. Gather? Async?
+progressbar alpha? Over? gets forecast 
+write and read class 
+
+'''
 
 
-def get_url(data_file):
-    with open(data_file, 'r', encoding='utf-8') as f:
-        for line in f:
-            yield line
-
-
-async def get_data(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0"
-    }
-    data = requests.get(url=url, headers=headers).text
-    return data
-
-def get_info(html):
-    soup = BeautifulSoup(html, "lxml")
-    forecast = soup.findAll('div', attrs={'class': 'forecast'})
-    forecast[0].findAll('h3')
-    print(1)
+class Gui(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.win_x = 700
+        self.win_y = 500
+        self.setWindowTitle("Погода по районам Владивостока")
+        self.setMinimumSize(QSize(self.win_x, self.win_y))
 
 
 async def main():
-    csv_data = "./towns.csv"
-    with open(csv_data, 'r', encoding='utf-8') as csv_file:
-        lines = csv.reader(csv_file)
-        next(lines)
-        for line in lines:
-            html = await get_data(line[2])
-            get_info(html)
-
-
-
-
-
-
+    app = QApplication([])
+    window = Gui()
+    window.show()
+    app.exec()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
