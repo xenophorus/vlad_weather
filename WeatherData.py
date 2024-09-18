65092fcdbfbc7c5aeeea00a9c9d942b32a3ea850
xenophorus <xenophorus@gmail.com>
2024-09-19 03:25:01 +1000

    1

--- WeatherData.py
+++ WeatherData.py
@@ -1,3 +1,4 @@
+from operator import index
 from tempfile import tempdir
 
 import requests
@@ -12,9 +13,11 @@ from bs4 import BeautifulSoup
 
 class WeatherData:
     locale.setlocale(locale.LC_ALL, "ru_RU")
+
     def __init__(self, days: int, nights: bool):
         self.days = days
         self.nights = nights
+        self.forecast = dict()
 
     def run(self):
         asyncio.run(self.get_weather_data())
@@ -25,35 +28,45 @@ class WeatherData:
         }
         data = requests.get(url=url, headers=headers).text
         return data
-    '''
-    время года,дата,день,город,Город цифровое значение,
-    температура +/- C,температура ощущается, погодные условия,погодные условия текст,
-    влажность%, Ветер текст, ветер (м.с), давление
-    '''
-    def get_info(self, html, region_num, region):
-        forecast = dict()
+
+    def get_info_nights(self, html, region_num, region):
+        day_date = date.today() + timedelta(days=self.days)
+        indexes = [0, 2]
         soup = BeautifulSoup(html, "lxml")
         forecast = soup.findAll('table', attrs={'class': 'six-hour'})
-        for day in range(1, self.days + 1):
-            date_forecast = dict()
-            day_date = date.today() + timedelta(days=day)
-            season = self._get_season(day_date.month)
-            date_ = f"{day_date.day} {self._get_month(day_date.month)}"
-            weekday = calendar.day_name[day_date.weekday()]
-            region_num: int = region_num #!
-            region_name : region #!
-            temp_real: str
-            temp_feel: str
-            weather_num: int
-            weather_text: str
-            humidity: str
-            wind_text: str
-            wind_speed: str
-            pressure: str
+        for day_time in [1, self.days]:
+            day = forecast[day_time]
+            weather = [x.find("div").text for x in
+                       day.findAll("tr", attrs={"class": "weather"})[0].findAll("td")[1:]]
+            precipitation = [x.text.strip() for x in
+                             day.findAll("tr", attrs={"class": "precipitation"})[0].findAll("td")[1:]]
+            temperature = [x.find("div", attrs={"class": "show-for-small-only"}).text for x in
+                           day.findAll("tr", attrs={"class": "temperature"})[0].findAll("td")[1:]]
+            feeled_temperature = [x.find("div", attrs={"class": "show-for-small-only"}).text for x in
+                                  day.findAll("tr", attrs={"class": "feeled-temperature"})[0].findAll("td")[1:]]
+            wind = [list(x.find("div").stripped_strings) for x in
+                    day.findAll("tr", attrs={"class": "wind"})[0].findAll("td")[1:]]
+            pressure = [list(x.stripped_strings) for x in
+                        day.findAll("tr", attrs={"class": "pressure"})[0].findAll("td")[1:]]
+            humidity = [x.string for x in day.findAll("tr", attrs={"class": "humidity"})[0].findAll("td")[1:]]
 
+            for i in indexes:
+                self.forecast.update({day_date.strftime(f"%Y-%m-%d_{i}"): {
+                    "season": self._get_season(day_date.month),
+                    "date": f"{day_date.day} {self._get_month(day_date.month)}",
+                    "weekday": calendar.day_name[day_date.weekday()],
+                    "region_num": region_num,
+                    "region_name": region,
+                    "temp_real": temperature[i],
+                    "temp_feel": feeled_temperature[i],
+                    "weather_num": "",
+                    "weather_text": weather[i],
+                    "humidity": humidity[i],
+                    "wind_text": wind[i][0],
+                    "wind_speed": wind[i][1],
+                    "pressure": f"{pressure[i][0]}мм.рт.ст."
+                }})
 
-
-            #
             print(1)
 
     async def get_weather_data(self):
@@ -62,10 +75,10 @@ class WeatherData:
             url = line.get("url")
             if self.nights:
                 html = await self.get_data(url + "/.week")
-                weather_dict = self.get_info(html, line.get("num"), line.get("town"))
+                self.get_info_nights(html, line.get("num"), line.get("town"))
             else:
                 html = await self.get_data(url + "/.14days")
-                weather_dict = self.get_info(html, line.get("num"), line.get("town"))
+                self.get_info(html, line.get("num"), line.get("town"))
 
     def _get_season(self, month) -> int:
         if month < 3 or month == 12:
